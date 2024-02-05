3c1a9265725d088678ed0c623171a95603f2fe35
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-05 16:19:56 -0400

    basic enter and backspace working

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,28 @@ function keyClick(e){
     const currentGuess = game.dataset.currentGuess;
     const turn = game.dataset.turn;
     
-    if (target.tagName === 'BUTTON' && target.innerText !== 'ENTER' && target.innerText !== '<' && currentGuess.length < 5){
-        const game = document.getElementById('game');
-        // document.getElementById('guess-1').children[1].innerText
-        //.innerText
-        if (currentGuess){
-            document.getElementById(`guess-${turn}`).children[currentGuess.length].innerText = target.innerText; 
-        }else{
-            document.getElementById(`guess-${turn}`).children[0].innerText = target.innerText; 
+    if (target.tagName === 'BUTTON'){
+        if (target.innerText !== 'ENTER' && target.innerText !== '<' && currentGuess.length < 5){
+            const game = document.getElementById('game');
+            if (currentGuess){
+                document.getElementById(`guess-${turn}`).children[currentGuess.length].innerText = target.innerText; 
+            }else{
+                document.getElementById(`guess-${turn}`).children[0].innerText = target.innerText; 
+            }
+            game.dataset.currentGuess = currentGuess + target.innerText  
+        } else if (target.innerText === '<'){
+            if (currentGuess){
+                console.log(document.getElementById(`guess-${turn}`).children[currentGuess.length-1])
+                document.getElementById(`guess-${turn}`).children[currentGuess.length-1].innerText = '';
+                game.dataset.currentGuess = currentGuess.slice(0, -1);
+            }
+        }else if (target.innerText === 'ENTER'){
+          if (currentGuess.length === 5){
+                game.dataset.turn = Number(turn) + 1;
+                game.dataset.currentGuess = '';            
+            }
         }
-        game.dataset.currentGuess = currentGuess + target.innerText  
-        console.log(game.dataset.currentGuess);
+
     }
 }
 
