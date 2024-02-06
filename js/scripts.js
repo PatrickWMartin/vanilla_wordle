79ff41f24ed8f891b59abaf91e1754eb177d0afe
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-06 08:02:21 -0400

    Put turn and current guess variables in the javascript for some weird reason I can't explain I thought they had to live in the html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
+let turn = 1;
+let currentGuess = "";
+
 function keyClick(e){
     const target = e.target;
-    const currentGuess = game.dataset.currentGuess;
-    const turn = game.dataset.turn;
     
     if (target.tagName === 'BUTTON'){
         if (target.innerText !== 'ENTER' && target.innerText !== '<' && currentGuess.length < 5){
-            const game = document.getElementById('game');
+            document.getElementById(`guess-${turn}`).children[currentGuess.length].innerText = target.innerText; 
+            currentGuess += target.innerText;
+        }else if (target.innerText === '<'){
             if (currentGuess){
-                document.getElementById(`guess-${turn}`).children[currentGuess.length].innerText = target.innerText; 
-            }else{
-                document.getElementById(`guess-${turn}`).children[0].innerText = target.innerText; 
-            }
-            game.dataset.currentGuess = currentGuess + target.innerText  
-        } else if (target.innerText === '<'){
-            if (currentGuess){
-                console.log(document.getElementById(`guess-${turn}`).children[currentGuess.length-1])
                 document.getElementById(`guess-${turn}`).children[currentGuess.length-1].innerText = '';
-                game.dataset.currentGuess = currentGuess.slice(0, -1);
+                currentGuess = currentGuess.slice(0, -1);
             }
         }else if (target.innerText === 'ENTER'){
-          if (currentGuess.length === 5){
-                game.dataset.turn = Number(turn) + 1;
-                game.dataset.currentGuess = '';            
+            if (currentGuess.length === 5){
+                turn+=1 
+                currentGuess = '';            
             }
         }
-
     }
 }
 
